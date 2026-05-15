d91364ac3ca1619a245da795df1cc3c4368aa22c
badjoke-lab <badjoke.lab@gmail.com>
2026-05-15 10:08:04 +0900

    Configure Astro for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://badjoke-lab.github.io',
-  base: '/mintedandgone'
+  site: 'https://mintedandgone.pages.dev'
 });
